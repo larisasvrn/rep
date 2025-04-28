60b8c42085f78833f7eb676c9453731d3d82e40e
larisasvrn <107367059+larisasvrn@users.noreply.github.com>
2025-04-28 22:59:30 +0300

    Реализация алгоритма DFS

---
+++ dfs.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+
+def dfs(graph, start, end=None):
+    visited = set()
+    stack = [(start, 0)]  # (vertex, distance)
+    path_length = None
+
+    while stack:
+        vertex, distance = stack.pop()
+        if vertex == end:
+            path_length = distance
+        if vertex not in visited:
+            visited.add(vertex)
+            for neighbor in graph[vertex]:
+                stack.append((neighbor, distance + 1))
+
+    return list(visited), path_length
+
+
+# Пример использования
+edges = [(4, 2), (1, 3), (2, 4)]
+graph = defaultdict(list)
+for u, v in edges:
+    graph[u].append(v)
+    graph[v].append(u)
+
+print(dfs(graph, 1))  # Обход в глубину
+print(dfs(graph, 2, 4))  # Длина пути от 2 до 4 -> 1
